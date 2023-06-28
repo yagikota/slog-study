4efbfdefaa4ef70d7142e4d0f3a95daa2945e4c2
Kota <mdryzk64smsh@gmail.com>
2023-06-28 23:52:55 +0900

    zap

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"time"
 
-	"github.com/rs/zerolog"
+	"go.uber.org/zap"
 )
 
 func main() {
-	logger := zerolog.New(os.Stdout)
-	logger.Info().
-		Str("name", "John").
-		Int("age", 9).
-		Msg("hello from zerolog")
+	// returns zap.Logger, a strongly typed logging API
+	logger := zap.Must(zap.NewProduction())
+
+	defer logger.Sync()
+
+	start := time.Now()
+
+	logger.Info("Hello from zap Logger",
+		zap.String("name", "John"),
+		zap.Int("age", 9),
+		zap.String("email", "john@gmail.com"),
+	)
+
+	// convert zap.Logger to zap.SugaredLogger for a more flexible and loose API
+	// that's still faster than most other structured logging implementations
+	sugar := logger.Sugar()
+	sugar.Warnf("something bad is about to happen")
+	sugar.Errorw("something bad happened",
+		"error", fmt.Errorf("oh no"),
+		"answer", 42,
+	)
+
+	// you can freely convert back to the base `zap.Logger` type at the boundaries
+	// of performance-sensitive operations.
+	logger = sugar.Desugar()
+	logger.Warn("the operation took longer than expected",
+		zap.Int64("time_taken_ms", time.Since(start).Milliseconds()),
+	)
 }
