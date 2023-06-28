8822f627afeeb8902c56e7fd30f25966073dd18b
Kota <mdryzk64smsh@gmail.com>
2023-06-28 23:49:14 +0900

    zerolog

--- go.mod
+++ go.mod
@@ -2,13 +2,10 @@ module github.com/yagikota/slog-study
 
 go 1.20
 
-require (
-	github.com/fatih/color v1.15.0
-	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
-)
+require go.uber.org/zap v1.24.0
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,38 +3,13 @@ package main
 import (
 	"os"
 
-	"golang.org/x/exp/slog"
+	"github.com/rs/zerolog"
 )
 
-// User does not implement `LogValuer` here
-type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-}
-
-// implement the `LogValuer` interface
-func (u *User) LogValue() slog.Value {
-    return slog.GroupValue(
-        slog.String("id", u.ID),
-        slog.String("name", u.FirstName+" "+u.LastName),
-    )
-}
-
-
 func main() {
-	handler := slog.NewJSONHandler(os.Stdout, nil)
-	logger := slog.New(handler)
-
-	u := &User{
-		ID:        "user-12234",
-		FirstName: "Jan",
-		LastName:  "Doe",
-		Email:     "jan@example.com",
-		Password:  "pass-12334",
-	}
-
-	logger.Info("info", "user", u)
+	logger := zerolog.New(os.Stdout)
+	logger.Info().
+		Str("name", "John").
+		Int("age", 9).
+		Msg("hello from zerolog")
 }
