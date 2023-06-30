692e8c79ff5f8b9ca3754cd13852d7aa047bd76e
Kota <mdryzk64smsh@gmail.com>
2023-07-01 01:36:15 +0900

    prettyJSONHandler for nest json

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/yagikota/slog-study
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+require (
+	github.com/fatih/color v1.15.0
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,38 +2,93 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"io"
+	"log"
+	"os"
+	"strings"
 
 	"golang.org/x/exp/slog"
 )
 
+// slog.Handlerをラップして独自のハンドラーを定義
 type prettyJSONHandler struct {
 	slog.Handler
+	l      *log.Logger
 	indent int
 }
 
+func newPrettyHandler(w io.Writer, opts prettyJSONHandlerOptions) *prettyJSONHandler {
+	return &prettyJSONHandler{
+		Handler: slog.NewJSONHandler(w, &opts.slogOps),
+		l:       log.New(w, "", 0),
+		indent:  opts.indent,
+	}
+}
+
+// 独自のハンドラーに対するオプション
 type prettyJSONHandlerOptions struct {
 	slogOps slog.HandlerOptions
 	indent  int
 }
 
-func newPrettyHandler(w io.Writer, opts *prettyJSONHandlerOptions) *prettyJSONHandler {
-	return &prettyJSONHandler{
-		Handler: slog.NewJSONHandler(w, &opts.slogOps),
-		indent:  opts.indent,
+func makeFields(fields map[string]interface{}, a slog.Attr) {
+	value := a.Value.Any()
+	if _, ok := value.([]slog.Attr); !ok {
+		fields[a.Key] = value
+		return
 	}
+
+	innerFields := make(map[string]interface{}, len(value.([]slog.Attr)))
+	for _, attr := range value.([]slog.Attr) {
+		makeFields(innerFields, attr)
+	}
+	fields[a.Key] = innerFields
 }
 
-func (*prettyJSONHandler) Handle(_ context.Context, r slog.Record) error {
- // todo
+func (h *prettyJSONHandler) Handle(_ context.Context, r slog.Record) error {
+	fields := make(map[string]interface{}, r.NumAttrs())
+	fields["time"] = r.Time
+	fields["level"] = r.Level
+	fields["message"] = r.Message
+	r.Attrs(func(a slog.Attr) bool {
+		makeFields(fields, a)
+		return true
+	})
+
+	b, err := json.MarshalIndent(fields, "", strings.Repeat(" ", h.indent))
+	if err != nil {
+		return err
+	}
+
+	h.l.Println(string(b))
+
+	return nil
 }
 
 func main() {
-	handler := newPrettyHandler()
+	ops := prettyJSONHandlerOptions{
+		slogOps: slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+		indent: 4,
+	}
+	handler := newPrettyHandler(os.Stdout, ops)
 	logger := slog.New(handler)
+
 	logger.Debug("Debug message")
 	logger.Info("Info message")
 	logger.Warn("Warning message")
 	logger.Error("Error message")
 
+	logger.Info(
+		"image uploaded",
+		slog.Int("id", 23123),
+		slog.Group("properties",
+			slog.Int("width", 4000),
+			slog.Group("properties",
+				slog.Int("height", 3000),
+				slog.String("format", "jpeg")),
+		),
+	)
 }
